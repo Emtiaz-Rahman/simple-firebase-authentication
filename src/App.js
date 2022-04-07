831d18f4ec6cc0d9d75bd7d3a09bb83459821cd8
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-04-07 11:40:04 +0600

    error solution done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+
+const auth = getAuth();
 
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn=()=>{
+    signInWithPopup(auth,provider)
+    .then(result=>{
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error=>{
+      console.log('error')
+    })
+  }
   return (
     <div className="App">
-      
+      <button onClick={ handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
