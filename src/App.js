bba0dc587ce6a8443de6bcc4a088cfe1cd06a341
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-04-08 23:42:37 +0600

    submit btn done

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ import { getAuth } from "firebase/auth";
 const auth = getAuth(app);
 
 function App() {
-  
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value)
+  }
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value)
+  }
+  const handleFormSbmit = event => {
+    console.log('from submit')
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <form>
-        <input type="text" />
-        <input type="password" name="" id="" />
+      <form onSubmit={handleFormSbmit}>
+        <input onBlur={handleEmailBlur} type="email" name="" id="" />
+        <br />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
+        <br />
+        <input type="submit" value="Login" />
       </form>
-      
+
     </div>
   );
 }
